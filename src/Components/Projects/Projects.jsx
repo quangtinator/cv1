622cdd56804db41ead2ad8f9fdbf614dd6e5b8bd
quangtinator <quang.phung@rwth-aachen.de>
2023-07-19 14:46:47 +0200

    add mqtt project

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Projects.css";
-import { FaReact, FaDocker } from "react-icons/fa";
+import { FaReact, FaDocker, FaPython } from "react-icons/fa";
 import {
   SiHtml5,
   SiExpress,
   SiMongodb,
   SiNodedotjs,
+  SiEclipsemosquitto
 } from "react-icons/si";
 import { DiCss3 } from "react-icons/di";
 import { IoLogoJavascript } from "react-icons/io";
@@ -43,6 +44,36 @@ export const Projects = () => {
                     <SiNodedotjs />
                     <SiExpress />
                     <SiMongodb />
+                    <FaDocker />
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+
+          <div className="projects_container" data-aos="fade-right">
+            <a
+              href="https://github.com/quangtinator/prime_mqtt"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="project">
+                <div className="project_videocontainer">
+                  <div>
+                    <img
+                      src="https://i.ibb.co/PgHmng9/docker-compose-up.png"
+                      alt="Message Broker"
+                    />
+                  </div>
+                </div>
+                <div className="project_information">
+                  <h2>Python MQTT Prime</h2>
+                  <p>
+                  Python MQTT Prime is a small demo project for Mosquitoo MQTT.
+                  </p>
+                  <div>
+                    <FaPython/>
+                    <SiEclipsemosquitto/>
                     <FaDocker />
                   </div>
                 </div>
